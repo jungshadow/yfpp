115517893c625f464c86c8b4465a212e577c38ce
Jay Johnson <jay@jhjdesigns.com>
2012-11-02 10:30:32 -0400

    Results styles and restart link
    Results styles and restart link

--- yfpp/dev_static/js/script.js
+++ yfpp/dev_static/js/script.js
@@ -1,26 +1,15 @@
-/* Author: 
+$(".toggle-button").click(function () {
+        $(this).toggleClass("closed open").next('.toggle-container').slideToggle();
 
-*/
+    });
 
+    $('.toggle-button, .toggle-container').click(function (e) {
+        e.stopPropagation();
+    });
+    $(document).click(function () {
+           $(".toggle-container").slideUp('slow', function () {
+            // Animation complete.
+            
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
