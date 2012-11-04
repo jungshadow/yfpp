6dc1775d018817dd228c7d4b4c8fbfb172f9bdec
Nick Catalano <nickcatal@gmail.com>
2012-11-03 21:27:56 -0400

    Twitter tracking on master

--- yfpp/dev_static/js/script.js
+++ yfpp/dev_static/js/script.js
@@ -14,3 +14,21 @@ $(".toggle-button").click(function (e) {
         });
 
     });
+function trackTwitter(intent_event) {
+  if (intent_event) {
+    var opt_pagePath;
+    if (intent_event.target && intent_event.target.nodeName == 'IFRAME') {
+          opt_target = extractParamFromUri(intent_event.target.src, 'url');
+    }
+    _gaq.push(['_trackSocial', 'twitter', 'tweet', opt_pagePath]);
+  }
+}
+
+//Wrap event bindings - Wait for async js to load
+twttr.ready(function (twttr) {
+  //event bindings
+  twttr.events.bind('tweet', trackTwitter);
+  twttr.events.bind('follow', function(event) {
+      _gaq.push(['_trackSocial', 'twitter', 'follow', event.data.screen_name]);
+      });
+});
