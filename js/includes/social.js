bcd1caa6afc7cc7aa7e8c34e8a52a958bd97da48
Nick Catalano <nickcatal@gmail.com>
2014-10-16 22:53:27 -0500

    Initial analytics work

--- js/includes/social.js
+++ js/includes/social.js
@@ -1,31 +1,40 @@
-
+/*
+Your Fucking Polling Place
+Social JavaScript
+by Nick Catalano
+www.nickcatalano.com
+*/
 
 (function(window, amplify, $){
 
+    (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-    window.fbAsyncInit = function() {
+    window.twttr = (function (d,s,id) {
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+    js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f); } });
+    }(document, "script", "twitter-wjs"));
+
+    // Use $.Deferred() to allow extra things to run when Facebook first loads
+    // From http://johnkpaul.tumblr.com/post/32087902037/handling-fbasyncinit-use-deferred
+    window.fbAsyncInit = function(){
         FB.init({
           appId      : '382719335136154',
           xfbml      : true,
           version    : 'v2.1'
         });
+        window.fbAsyncInit.fbLoaded.resolve();
     };
-    /* jshint ignore:start */
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    window.fbAsyncInit.fbLoaded = $.Deferred();
 
-    window.twttr = (function (d,s,id) {
-        var t, js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-        js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-        return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f); } });
-    }(document, "script", "twitter-wjs"));
-    /* jshint ignore:stop */
+
 
     amplify.subscribe("resultsRendered", function() {
         if ('widgets' in window.twttr) {
