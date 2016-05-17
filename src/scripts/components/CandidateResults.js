5aeb6db6f36e881543e1d86ec85b80c38d40669a
Jay Johnson <jay@jhjdesigns.com>
2016-05-16 23:18:12 -0400

    adds candidate results component

---
+++ src/scripts/components/CandidateResults.js
@@ -0,0 +1,41 @@
+// Import dependencies 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+/**
+ * Candidate Results Component
+ *
+ * @class CandidateResults
+ * @extends React.Component
+ */
+ class CandidateResults extends React.Component {
+
+	/**
+	 * Renders candidate results list items
+	 *
+	 * @method render
+	 * @return {object} candidate results component markup
+	 */
+	 render() {
+
+	 	let key = this.props.can;
+	 	const candidate = this.props.candidate;
+
+	 	if (!candidate.name) {
+	 		return;
+	 	}
+
+	 	return (
+	 		<li className="candidate">
+	 			<div>{candidate.name}
+	 			<strong>{candidate.party}</strong></div>
+	 		</li>
+	 		)
+	 }
+
+	};
+
+	export default CandidateResults;
