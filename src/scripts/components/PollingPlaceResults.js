6fb42a4270e49051e37472ddedd0d6cfbf9297f9
Jay Johnson <jay@jhjdesigns.com>
2016-05-21 17:30:21 -0400

    adds card markup to PollingPlaceResults

--- src/scripts/components/PollingPlaceResults.js
+++ src/scripts/components/PollingPlaceResults.js
@@ -1,4 +1,4 @@
-// Import dependencies 
+<div></div>// Import dependencies 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -22,14 +22,38 @@ class PollingPlaceResults extends React.Component {
 		const locations = this.props.pollingLocations;
 
 		return (
-			<li className="results_pollingplace">
-				<h3 className="hdg hdg_3">{locations.address.locationName}</h3>
-				<div>{locations.address.city}</div>
-				<div>{locations.address.line1}</div>
-				<div>{locations.address.line2}</div>
-				<div>{locations.address.state}</div>
-				<div>{locations.address.zip}</div>
-				<div>{locations.pollingHours}</div>
+			<li>
+				<div className="card">
+					<div className="card-bd">
+						<h3 className="hdg hdg_3">{locations.address.locationName}</h3>
+						<div>{locations.address.line1}</div>
+						<div>{locations.address.line2}</div>
+						<div>{locations.address.city}, {locations.address.state} {locations.address.zip}</div>
+						<div>{locations.pollingHours}</div>
+					</div>
+					<div className="card-ft"><ul className="actions">
+						<li className="actions-item">
+							<span className="actions-item-title">Share</span>
+							<div className="actions-item-links">
+							<ul className="hList">
+								<li>
+									<a href="#">Twitter</a>
+								</li>
+								<li>
+									<a href="#">Facebook</a>
+								</li>
+							</ul></div>
+						</li>
+						<li className="actions-item">
+							<span className="actions-item-title">Map It</span>
+							<div className="actions-item-links">
+							
+									<a href="#">Map</a>
+								
+							</div>
+						</li>
+					</ul></div>
+				</div>
 			</li>
 			)
 	}
