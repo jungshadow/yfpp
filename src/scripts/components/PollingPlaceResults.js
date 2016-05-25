fd1e95c71facd35720ec5b022b691fb4c4c8a781
Jay Johnson <jay@jhjdesigns.com>
2016-05-24 20:03:14 -0400

    adds actions to cards markup

--- src/scripts/components/PollingPlaceResults.js
+++ src/scripts/components/PollingPlaceResults.js
@@ -1,6 +1,8 @@
-<div></div>// Import dependencies 
+// Import dependencies 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Actions from './Actions'
 
 
 /**
@@ -31,28 +33,9 @@ class PollingPlaceResults extends React.Component {
 						<div>{locations.address.city}, {locations.address.state} {locations.address.zip}</div>
 						<div>{locations.pollingHours}</div>
 					</div>
-					<div className="card-ft"><ul className="actions">
-						<li className="actions-item">
-							<span className="actions-item-title">Share</span>
-							<div className="actions-item-links">
-							<ul className="hList">
-								<li>
-									<a href="#">Twitter</a>
-								</li>
-								<li>
-									<a href="#">Facebook</a>
-								</li>
-							</ul></div>
-						</li>
-						<li className="actions-item">
-							<span className="actions-item-title">Map It</span>
-							<div className="actions-item-links">
-							
-									<a href="#">Map</a>
-								
-							</div>
-						</li>
-					</ul></div>
+					<div className="card-ft">
+						<Actions />
+					</div>
 				</div>
 			</li>
 			)
