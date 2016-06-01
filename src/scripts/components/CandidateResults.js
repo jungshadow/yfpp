efab77aee7e507171222b41df78fada2cadcd7a6
Jay Johnson <jay@jhjdesigns.com>
2016-06-01 00:48:41 -0400

    adds social links to candidate results

--- src/scripts/components/CandidateResults.js
+++ src/scripts/components/CandidateResults.js
@@ -13,6 +13,26 @@ import ReactDOM from 'react-dom';
  */
  class CandidateResults extends React.Component {
 
+
+    renderSocial() {
+
+        this.socialLinks = [];
+
+        let i = 0;
+
+        if (this.props.candidate.channels) {
+
+            this.props.candidate.channels.forEach(function(channel) {
+                this.socialLinks.push (<li key={i}><a href={channel.id} className={'actionLink actionLink_' + channel.type.toLowerCase()}>{channel.type}</a></li>);
+
+                i++; 
+
+            }.bind(this));           
+
+        }
+
+        return this.socialLinks;
+    }
     /**
      * Renders candidate results list items
      *
@@ -27,10 +47,19 @@ import ReactDOM from 'react-dom';
             return;
         }
 
+
+
         return (
             <li className="candidate">
-                <div>{candidate.name}
-                <strong>{candidate.party}</strong></div>
+                <div className="split">
+                    <div className="split-lft">
+                        <strong className="txt mix-txt_blue">{candidate.name}</strong>
+                        {candidate.party ? <span> - {candidate.party}</span> : ''}
+                    </div>
+                    <div className="split-rgt">
+                        <ul className="hList">{this.renderSocial()}</ul>
+                    </div>
+                </div>
             </li>
             )
      }
