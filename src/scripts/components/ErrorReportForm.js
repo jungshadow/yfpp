b50ee09ed0014fb33c3cda74ec67a7ebe7803867
Jay Johnson <jay@jhjdesigns.com>
2016-06-06 22:52:04 -0400

    updates markup with errorLink class

--- src/scripts/components/ErrorReportForm.js
+++ src/scripts/components/ErrorReportForm.js
@@ -21,8 +21,8 @@ class ErrorReportForm extends React.Component {
     * @return none
     */ 
     onClickHandler(e) {
-	e.preventDefault();
-	this.refs.errorFeedbackForm.submit();
+    	e.preventDefault();
+    	this.refs.errorFeedbackForm.submit();
     }
 
     /**
@@ -36,8 +36,8 @@ class ErrorReportForm extends React.Component {
 	const electionInfo = this.props.electionInfo;
 
 	return (
-            <div className="group">
-                <a href="#" id="error-feedback-link" data-state={normalizedAddress.state} onClick={this.onClickHandler}><strong>This fucking thing wrong? Report it here.</strong></a>
+            <div className="tabs-panel-errorLink">
+                <a className="link" href="#" id="error-feedback-link" data-state={normalizedAddress.state} onClick={this.onClickHandler}>This fucking thing wrong? <strong>Report it here.</strong></a>
                 <form className="hidden" style={{display: 'none'}} method="post" action="https://voter-info-tool.appspot.com/feedback" ref="errorFeedbackForm" id="error-feedback-form">
                     <input type="hidden" value={electionInfo.id} name="electionId" />
                     <input type="hidden" value={helpers.concatStreetAddress(normalizedAddress)} name="address" />
