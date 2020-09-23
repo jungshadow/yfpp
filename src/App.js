07414b6ada80ac8544bc35f7b9e22124f31cef19
Jay Johnson <jay@jayjohnson.io>
2020-09-22 21:46:19 -0400

    adds redirect if results exist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 
 import { appReducer, initialState, AppContext, DispatchContext } from 'appReducer';
 import Site from 'components/Site/Site';
+import { Redirect } from 'react-router-dom';
 
 const App = () => {
     const [state, dispatch] = useReducer(appReducer, initialState);
@@ -9,6 +10,7 @@ const App = () => {
     return (
         <AppContext.Provider value={state}>
             <DispatchContext.Provider value={dispatch}>
+                {state.isActive && <Redirect to="/polling-place" />}
                 <Site />
             </DispatchContext.Provider>
         </AppContext.Provider>
