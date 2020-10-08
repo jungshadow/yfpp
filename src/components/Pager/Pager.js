1c274b9e971576573699baaa99be1c278627762b
Jay Johnson <jay@jayjohnson.io>
2020-10-08 00:38:20 -0400

    more animation tweaks

--- src/components/Pager/Pager.js
+++ src/components/Pager/Pager.js
@@ -12,7 +12,7 @@ const Pager = (props) => {
         show: {
             opacity: 1,
             transition: {
-                staggerChildren: 0.1,
+                staggerChildren: 0.075,
             },
         },
     };
