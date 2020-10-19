b2f304a8e8ac888ac8d0039d7a221dd2805918e7
Jay Johnson <jay@jayjohnson.io>
2020-10-18 23:11:08 -0400

    cleanup

--- src/components/KitchenSink/KitchenSink.js
+++ src/components/KitchenSink/KitchenSink.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import './kitchenSink.scss';
 
-const KitchenSink = ({children}) => {
-    return <div className="kitchenSink">{children}</div>;
+const KitchenSink = ({children, isReversed, isCentered}) => {
+    const getKitchenSinkClassnames = () => {
+        return classnames({
+            kitchenSink: true,
+            'kitchenSink--reversed': isReversed,
+            'kitchenSink--centered': isCentered
+        });
+    };
+    return <div className={getKitchenSinkClassnames()}>{children}</div>;
 };
 
-KitchenSink.propTypes = {children: PropTypes.oneOfType([PropTypes.node, PropTypes.string])};
+KitchenSink.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.string])
+};
 
 export default KitchenSink;
